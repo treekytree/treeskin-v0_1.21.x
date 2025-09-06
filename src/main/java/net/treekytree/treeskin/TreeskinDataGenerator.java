b098333fa225e5d76eacb26fa796ba9d2f3256e5
treekytree <treekytree@gmail.com>
2025-09-06 23:12:21 +0200

    test

---
+++ src/main/java/net/treekytree/treeskin/TreeskinDataGenerator.java
@@ -0,0 +1,11 @@
+package net.treekytree.treeskin;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TreeskinDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/treekytree/treeskin/treeskinClient.java
@@ -0,0 +1,10 @@
+package net.treekytree.treeskin;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class treeskinClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
